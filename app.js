833126a0afd67e2e8947986a1c7e2c7bdfb9c5bb
Paula Suarez <psuarez@dickeys2.local>
2018-11-28 08:34:53 -0600

    Adding updates to .js file, again.

--- app.js
+++ app.js
@@ -32,51 +32,74 @@
 //Class example #3-Methods and Properties
 
 //class Shape {
-    // Getter property
-    //get area() {
-       // return this.calculateArea();
-    //}
+// Getter property
+// get area() {
+//  return this.calculateArea();
+//}
 
-    //constructor(width, height) {
-      //  this.width = width;
-      //  this.height = height;
-   // }
+// constructor(width, height) {
+//  this.width = width;
+//  this.height = height;
+//}
 
-    //method #1
-  //  resize(width, height) {
-      //  this.width = width;
-      //  this.height = height;
-   // }
+//method #1
+// resize(width, height) {
+//  this.width = width;
+//  this.height = height;
+//  }
 
-    //method #2
-   // calculateArea() {
-      //  return this.width * this.height;
-   // }
+//method #2
+// calculateArea() {
+//  return this.width * this.height;
+//}
 //}
 
 //class Circle extends Shape {
-   // constructor(radius) {
-       // super(2 * radius, 2 * radius);
-   // }
+// constructor(radius) {
+//   super(2 * radius, 2 * radius);
+//  }
 
-   // resize(radius) {
-        //super.resize(2 * radius, 2 * radius);
-    //}
+// resize(radius) {
+//  super.resize(2 * radius, 2 * radius);
+// }
 
-   // calculateArea() {
-      //  const radius = this.width / 2;
+//  calculateArea() {
+//    const radius = this.width / 2;
 
-      //  return Math.PI * radius * radius;
-    //}
+//  return Math.PI * radius * radius;
+// }
 //}
 
 //const myCircle = new Circle(5);
 
 //console.log(myCircle.area);
 
+
 //Class example #4-Static Method
 
+class Shape {
+    // Getter property
+    get area() {
+        return this.calculateArea();
+    }
+
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    resize(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    calculateArea() {
+        return this.width * this.height;
+    }
+}
+
 class Circle extends Shape {
+    //static keyword defines a method on a class
     static diameter(circle) {
         return circle.width * 2;
     }
@@ -95,7 +118,6 @@ class Circle extends Shape {
         return Math.PI * radius * radius;
     }
 }
-
 
 const myCircle = new Circle(5);
 
